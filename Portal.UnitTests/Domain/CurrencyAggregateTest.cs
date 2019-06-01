3a143b946624a2bfda9c8e1ad35800030547e6e2
Ali Soltani <soltani1384@gmail.com>
2019-06-01 10:58:19 +0430

    imporoving domain unit tests.

--- Portal.UnitTests/Domain/CurrencyAggregateTest.cs
+++ Portal.UnitTests/Domain/CurrencyAggregateTest.cs
@@ -11,28 +11,43 @@ namespace Portal.UnitTests.Domain
 {
     public class CurrencyAggregateTest
     {
-        [Fact]
-        public void CurrencyDefinition_Invalid_Arguments()
+
+        [Theory]
+        [InlineData(-1, "FakeCountrytName", "FakeCurrencyType", "FakeAlphabeticCode", 1, 1)]
+        [InlineData(1, "", "FakeCurrencyType", "FakeAlphabeticCode", 1, 1)]
+        [InlineData(1, "FakeCountrytName", "", "FakeAlphabeticCode", 1, 1)]
+        [InlineData(1, "FakeCountrytName", "FakeCurrencyType", "", 1, 1)]
+        [InlineData(1, "FakeCountrytName", "FakeCurrencyType", "FakeAlphabeticCode", -1, 1)]
+        public void CurrencyDefinition_Invalid_Arguments(int currencyNumericCode, string country, string currencyType, string alphabeticCode, decimal exchangeRate, int userId)
         {
             //Act - Assert
-            Assert.Throws<PortalDomainException>(() => Currency.CurrencyDefinition(-1, "FakeCountrytName", "FakeCurrencyType", "FakeAlphabeticCode", 1, 1));
-            Assert.Throws<PortalDomainException>(() => Currency.CurrencyDefinition(1, "", "FakeCurrencyType", "FakeAlphabeticCode", 1, 1));
-            Assert.Throws<PortalDomainException>(() => Currency.CurrencyDefinition(1, "FakeCountrytName", "", "FakeAlphabeticCode", 1, 1));
-            Assert.Throws<PortalDomainException>(() => Currency.CurrencyDefinition(1, "FakeCountrytName", "FakeCurrencyType", "", 1, 1));
-            Assert.Throws<PortalDomainException>(() => Currency.CurrencyDefinition(1, "FakeCountrytName", "FakeCurrencyType", "FakeAlphabeticCode", -1, 1));
+            Assert.Throws<PortalDomainException>(() => Currency.CurrencyDefinition(currencyNumericCode, country, currencyType, alphabeticCode, exchangeRate, userId));
         }
 
-        [Fact]
-        public void UpdateCurrency_Invalid_Arguments()
+        [Theory]
+        [InlineData("NewFakeCountrytName", "NewFakeCurrencyType", "NewFakeAlphabeticCode", 10, 10)]
+        [InlineData("FakeCountrytName", "NewFakeCurrencyType", "NewFakeAlphabeticCode", 10, 10)]
+        [InlineData("FakeCountrytName", "FakeCurrencyType", "NewFakeAlphabeticCode", 10, 10)]
+        [InlineData("FakeCountrytName", "FakeCurrencyType", "FakeAlphabeticCode", 10, 10)]
+        [InlineData("FakeCountrytName", "FakeCurrencyType", "FakeAlphabeticCode", 1, 10)]
+        public void UpdateCurrency_Invalid_Arguments(string country, string currencyType, string alphabeticCode, decimal exchangeRate, int userId)
         {
-            // Arrange
-            var currency = Currency.CurrencyDefinition(1, "FakeCountrytName", "FakeCurrencyType", "FakeAlphabeticCode", 1, 1);
+            //Arrange
+            int currencyNumericCode = 1;
+            var currency = Currency.CurrencyDefinition(currencyNumericCode, "FakeCountrytName", "FakeCurrencyType", "FakeAlphabeticCode", 1, 1);
+            var currencyResultOfUpdating = Currency.CurrencyDefinition(currencyNumericCode, country, currencyType, alphabeticCode, exchangeRate, userId);
 
-            //Act - Assert
-            Assert.Throws<PortalDomainException>(() => currency.UpdateCurrency("", "FakeCurrencyType", "FakeAlphabeticCode", 1, 1));
-            Assert.Throws<PortalDomainException>(() => currency.UpdateCurrency("FakeCountrytName", "", "FakeAlphabeticCode", 1, 1));
-            Assert.Throws<PortalDomainException>(() => currency.UpdateCurrency("FakeCountrytName", "FakeCurrencyType", "", 1, 1));
-            Assert.Throws<PortalDomainException>(() => currency.UpdateCurrency("FakeCountrytName", "FakeCurrencyType", "FakeAlphabeticCode", -1, 1));
+            //Act
+            currency.UpdateCurrency(country, currencyType, alphabeticCode, exchangeRate, userId);
+
+            //Assert
+            Assert.Equal(currency.Country, currencyResultOfUpdating.Country);
+            Assert.Equal(currency.CurrencyType, currencyResultOfUpdating.CurrencyType);
+            Assert.Equal(currency.AlphabeticCode, currencyResultOfUpdating.AlphabeticCode);
+            Assert.Equal(currency.ExchangeRate, currencyResultOfUpdating.ExchangeRate);
+            Assert.Equal(currency.UserID, currencyResultOfUpdating.UserID);
         }
+
+
     }
 }
